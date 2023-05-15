eb94c4ae423f46882f5bc364c96f36bdcab439e9
qingpeng <qingpeng@u.nus.edu>
2023-05-15 14:40:57 +0800

    add data details function and implement the page without js

--- cool_dashboard/views/__init__.py
+++ cool_dashboard/views/__init__.py
@@ -2,7 +2,7 @@ from .login import webLogin, webLogout
 from .register import Register
 from .dashboard import Dashboard
 from .upload import Upload
-from .dataset_show import DatasetShow
+from .dataset_show import DatasetShow, DatasetDetail
 from .error import test_error_500
 from .utils import analyze_columns, test_request, test
 from .pass_request import pass_load, pass_read_col
--- cool_dashboard/views/dataset_show.py
+++ cool_dashboard/views/dataset_show.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User
+from django.http import JsonResponse
 from django.views import View
 from django.shortcuts import render, redirect
+from django.forms.models import model_to_dict
 from .pass_request import *
 
 from ..config import *
@@ -22,6 +24,27 @@ class DatasetShow(View):
             subset['set_detail'] = cube.set_details
             subset['num_records'] = cube.num_records
             context['datasets'][index] = subset
-        logger.info(context)
+        # logger.info(context)
         return render(request, "dataset-show.html", context)
 
+class DatasetDetail(View):
+    def get(self, request, set_id):
+        context = {}
+        cube = cube_details.objects.filter(user_id=request.user.id, file_id=set_id)
+        cube.values()
+        if not cube.exists():
+            return render(request, "error-500.html", {'error': "Could not find the dataset under this account."})
+        cube = cube[0]
+        logger.info(cube)
+        context['set_id'] = cube.file_id
+        context['set_name'] = cube.set_name
+        context['set_size'] = cube.cube_size
+        context['set_records'] = cube.num_records
+        context['set_ids'] = cube.num_ids
+        context['set_details'] = cube.set_details
+
+        context['set_info'] = model_to_dict(cube)
+        context['set_info'].pop('file_id')
+        context['set_info'].pop('user_id')
+        context['set_info'].pop('cube_size')
+        return render(request, "dataset-analysis.html", context)
