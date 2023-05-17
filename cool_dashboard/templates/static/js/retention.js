f6d85a926ae28ec8a2c138ac397a5637341b00b9
qingpeng <qingpeng@u.nus.edu>
2023-05-17 21:11:20 +0800

    add "add remove events" functionality and update "advanced" checkbox

--- cool_dashboard/templates/static/js/retention.js
+++ cool_dashboard/templates/static/js/retention.js
@@ -137,41 +137,6 @@ let cohortEventHtml = '<div class="event-container">' +
     '        </div>' +
     '        <div class="col-md-1 col-sm-1 col-xs-1" style="float:left; line-height:33px;">time(s)</div>' +
     '    </div>' +
-    '    <div class="row mb-4">' +
-    '        <label class="control-label col-md-3 col-sm-3 col-xs-12">Group by:</label>' +
-    '        <div class="col-md-3 col-sm-3 col-xs-12">' +
-    '            <select class="form-select select2-single data-table-yaml groupby">' +
-    '            </select>' +
-    '        </div>' +
-    '        <div class="col-md-3 col-sm-3 col-xs-12">' +
-    '            <label class="control-label float-left">Advanced:</label>' +
-    '            <div class="float-left">' +
-    '                <input style="margin-top:12px; margin-left: 8px;" class="advanced-checkbox adv" type="checkbox" name="" >' +
-    '            </div>' +
-    '        </div>' +
-    '    </div>' +
-    '    <div class="form-group advanced-container">' +
-    '        <div class="row">' +
-    '            <label class="control-label col-md-offset-1 col-md-3 col-sm-3 col-xs-12 ">Min:</label>' +
-    '            <div class="col-md-1 col-sm-1 col-xs-12">' +
-    '                <input type="text" placeholder="0" class="form-control adv1" placeholder="">' +
-    '            </div>' +
-    '            <label class="control-label col-md-1 col-sm-1 col-xs-12 ">Max:</label>' +
-    '            <div class="col-md-1 col-sm-1 col-xs-12">' +
-    '                <input type="text" placeholder="10" class="form-control adv2" placeholder="">' +
-    '            </div>' +
-    '        </div>' +
-    '        <div class="row" style="margin-top:6px">' +
-    '            <label class="control-label col-md-offset-1 col-md-3 col-sm-3 col-xs-12 ">Interval:</label>' +
-    '            <div class="col-md-1 col-sm-1 col-xs-12">' +
-    '                <input type="text" placeholder="20" class="form-control adv3" placeholder="">' +
-    '            </div>' +
-    '            <label class="control-label col-md-1 col-sm-1 col-xs-12 ">Log Scale:</label>' +
-    '            <div class="col-md-1 col-sm-1 col-xs-12">' +
-    '                <input style="margin-top:12px;" class="adv4" type="checkbox" name="" >' +
-    '            </div>' +
-    '        </div>' +
-    '    </div>' +
     '</div>';
 
 $(document).ready(function (){
@@ -226,3 +191,158 @@ function handleNewEvent() {
     eventsContainer.find('.event-remove.fa-minus-circle').show();
     updateEventLabelNumbering();
 }
+
+
+
+
+
+
+let globalFilterHtml = '<div class="row global-filter">'+
+'    <div class="col-md-3 col-sm-3 col-xs-4">'+
+'        <select class="form-select select2-single data-table-yaml"> </select>'+
+'    </div>'+
+'    <div class="col-md-6 col-sm-6 col-xs-6">'+
+'        <select class="form-select select2-single second-stage">'+
+'        </select>'+
+'    </div>'+
+'    <div class="col-sm-6 col-sm-6 col-xs-6 datetimepicker" style="display: none;">'+
+'        <div class="input-group date date1">'+
+'            <input type="text" class="form-control" />'+
+'            <span class="input-group-addon">'+
+'                <span class="glyphicon glyphicon-calendar"></span>'+
+'            </span>'+
+'        </div>'+
+'        <div class="input-group date date2">'+
+'            <input type="text" class="form-control" />'+
+'            <span class="input-group-addon">'+
+'                <span class="glyphicon glyphicon-calendar"></span>'+
+'            </span>'+
+'        </div>'+
+'    </div>'+
+'    <div class="col-sm-6 col-sm-6 col-xs-6 intRange" style="display: none;">'+
+'        <div class="col-md-5 col-sm-5 col-xs-5">'+
+'            <input type="text" class="form-control min ">'+
+'        </div>'+
+'        <div style="float:left; line-height:33px;">-</div>'+
+'        <div class="col-md-5 col-sm-5 col-xs-5">'+
+'            <input type="text" class="form-control max ">'+
+'        </div>'+
+'    </div>'+
+'    <div class="col-sm-1 col-sm-1 col-xs-1">'+
+'        <i class="fa fa-plus-circle globalFilter-add" style="padding-right:7px;" aria-hidden="true"></i>'+
+'        <i class="fa fa-minus-circle globalFilter-remove" style="padding-right:7px;" aria-hidden="true"></i>'+
+'    </div>'+
+'</div>';
+
+
+$(document.body).on('click', '.globalFilter-add.fa-plus-circle', function(d) {
+    var filterContainer = $("#global-filters-container");
+    var added = $(globalFilterHtml);
+    filterContainer.append(added);
+    // initTableYamlSelect2(added.find(".data-table-yaml"));
+    $(".globalFilter-empty-btn").hide();
+});
+
+$(document.body).on('click', '.globalFilter-remove.fa-minus-circle', function(d) {
+    var globalFiltersContainer = $("#global-filters-container");
+    $(this).parents(".global-filter").remove();
+    if(globalFiltersContainer.children(".global-filter").length < 1) {
+        $(".globalFilter-empty-btn").show();
+    } else {
+        $(".globalFilter-empty-btn").hide();
+    }
+
+});
+
+
+
+
+let eventFilterHtml = '<div class="row">'+
+'    <label class="text-center col-md-1 col-sm-1 col-xs-12">Their</label>'+
+'    <div class="select-multi-stage col-md-10 col-sm-10 col-xs-12">'+
+'        <div class="col-md-5 col-sm-5 col-xs-11 float-left">'+
+'            <select class="form-select select2-single data-table-yaml">'+
+'            </select>'+
+'        </div>'+
+'        <label class="text-center col-md-1 col-sm-1 col-xs-12 float-left">is</label>'+
+'        <div class="col-md-6 col-sm-6 col-xs-12 float-left">'+
+'            <select class="form-select second-stage"> </select>'+
+'        </div>'+
+'        <div class="col-sm-6 col-sm-6 col-xs-12 datetimepicker" style="display: none;">'+
+'            <div class="input-group date date1">'+
+'                <input type="text" class="form-control" />'+
+'                <span class="input-group-addon">'+
+'                    <span class="glyphicon glyphicon-calendar"></span>'+
+'                </span>'+
+'            </div>'+
+'            <div class="input-group date date2">'+
+'                <input type="text" class="form-control" />'+
+'                <span class="input-group-addon">'+
+'                    <span class="glyphicon glyphicon-calendar"></span>'+
+'                </span>'+
+'            </div>'+
+'        </div>'+
+'        <div class="col-sm-6 col-sm-6 col-xs-12 intRange" style="display: none;">'+
+'            <div class="col-md-5 col-sm-5 col-xs-5">'+
+'                <input type="text" class="form-control event-min " placeholder="1">'+
+'            </div>'+
+'            <div style="float:left; line-height:33px;">-</div>'+
+'            <div class="col-md-5 col-sm-5 col-xs-5">'+
+'                <input type="text" class="form-control event-max " placeholder="100">'+
+'            </div>'+
+'        </div>'+
+'    </div>'+
+'    <div class="col-md-1 col-sm-1 col-xs-1">'+
+'        <i class="fa fa-plus-circle eventFilter-add" style="padding-right:7px;" aria-hidden="true"></i>'+
+'        <i class="fa fa-minus-circle eventFilter-remove" style="display:none;padding-right:7px;" aria-hidden="true"></i>'+
+'    </div>'+
+'</div>'
+
+
+// Add eventFilter.
+$(document.body).on('click', '.eventFilter-add.fa-plus-circle', function(d) {
+    var container = $(this).parents(".multi-eventFilters");
+    var added = $(eventFilterHtml);
+    container.append(added);
+    // initTableYamlSelect2(added.find(".data-table-yaml"));
+
+    // If going from 1 to 2, show del btn
+    container.find(".eventFilter-remove").show();
+});
+// Remove eventFilter.
+$(document.body).on('click', '.eventFilter-remove.fa-minus-circle', function(d) {
+    var container = $(this).parents(".multi-eventFilters");
+    $(this).parent().parent().remove();
+
+    // If only 1 left, hide del btn
+    if(container.children().length < 2) {
+        container.find(".eventFilter-remove").hide();
+    }
+});
+
+
+
+
+// Show/Hide Filter Users.
+$("#filter-users-checkbox").on('change', function(d) {
+    if($(this).is(':checked')) {
+        $("#users-events-container").show();
+        $(".users-add-event-btn").show();
+
+    } else {
+        $("#users-events-container").hide();
+        $(".users-add-event-btn").hide();
+    }
+});
+
+
+// Show/Hide Advanced options.
+$(document.body).on('click', '.advanced-checkbox', function(d) {
+    // console.log($(this).parent().parents('.advanced-check').siblings())
+    var advancedForm = $(this).parents('.advanced-check').children(".advanced-container");
+    if($(this).is(':checked')) {
+        advancedForm.show();
+    } else {
+        advancedForm.hide();
+    }
+});
